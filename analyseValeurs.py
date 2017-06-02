f61df04f2ce2afe52d316f1c7972461ee9483a14
Tony <antoine.compagnie@gmail.com>
2017-06-02 02:46:56 +0200

    créé un tableau des emaBTC, attention il semble y avoir une erreur dans le test sur les ema

--- analyseValeurs.py
+++ analyseValeurs.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 
 # lire les donnees du fichier a chaque fois qu'il y a une nouvelle valeur et mettre les dernieres dans des tableaux 5,13 et la derniere dans lastValue
+# https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,DASH&tsyms=USD
 json_text = """
 [
 {"BTC":{"USD":2167.85},"ETH":{"USD":167.88},"DASH":{"USD":102.31}},
@@ -10,7 +11,28 @@ json_text = """
 {"BTC":{"USD":2251.47},"ETH":{"USD":177.71},"DASH":{"USD":109.12}},
 {"BTC":{"USD":2251.47},"ETH":{"USD":177.71},"DASH":{"USD":109.12}},
 {"BTC":{"USD":2251.47},"ETH":{"USD":177.71},"DASH":{"USD":109.12}},
-{"BTC":{"USD":2167.85},"ETH":{"USD":167.88},"DASH":{"USD":102.31}}
+{"BTC":{"USD":2167.85},"ETH":{"USD":167.88},"DASH":{"USD":102.31}},
+{"BTC":{"USD":2407.05},"ETH":{"USD":222.67},"DASH":{"USD":134.09}},
+{"BTC":{"USD":2405.47},"ETH":{"USD":222.86},"DASH":{"USD":134.09}},
+{"BTC":{"USD":2404.51},"ETH":{"USD":222.83},"DASH":{"USD":134.09}},
+{"BTC":{"USD":2404.23},"ETH":{"USD":222.86},"DASH":{"USD":134.09}},
+{"BTC":{"USD":2404.22},"ETH":{"USD":222.99},"DASH":{"USD":134.09}},
+{"BTC":{"USD":2403.5},"ETH":{"USD":222.85},"DASH":{"USD":134.09}},
+{"BTC":{"USD":2403.79},"ETH":{"USD":222.94},"DASH":{"USD":134.09}},
+{"BTC":{"USD":2403.74},"ETH":{"USD":222.8},"DASH":{"USD":133.96}},
+{"BTC":{"USD":2403.18},"ETH":{"USD":222.82},"DASH":{"USD":133.96}},
+{"BTC":{"USD":2403.37},"ETH":{"USD":222.82},"DASH":{"USD":133.96}},
+{"BTC":{"USD":2403.37},"ETH":{"USD":222.82},"DASH":{"USD":133.96}},
+{"BTC":{"USD":2403.37},"ETH":{"USD":223.09},"DASH":{"USD":133.96}},
+{"BTC":{"USD":2403.37},"ETH":{"USD":223.09},"DASH":{"USD":133.96}},
+{"BTC":{"USD":2404.15},"ETH":{"USD":223.09},"DASH":{"USD":133.96}},
+{"BTC":{"USD":2404.15},"ETH":{"USD":223.09},"DASH":{"USD":133.96}},
+{"BTC":{"USD":2404.31},"ETH":{"USD":223.02},"DASH":{"USD":133.96}},
+{"BTC":{"USD":2403.76},"ETH":{"USD":222.98},"DASH":{"USD":133.96}},
+{"BTC":{"USD":2403.76},"ETH":{"USD":222.98},"DASH":{"USD":133.96}},
+{"BTC":{"USD":2405.12},"ETH":{"USD":222.86},"DASH":{"USD":133.96}},
+{"BTC":{"USD":2404.76},"ETH":{"USD":222.91},"DASH":{"USD":133.99}},
+{"BTC":{"USD":2404.76},"ETH":{"USD":222.91},"DASH":{"USD":133.99}}
 ]
 """
 
@@ -36,10 +58,18 @@ fiveLastMean = np.array([["",'BTC','ETH','DASH'],
 # EMA: {Close - EMA(previous day)} x multiplier + EMA(previous day).
 # I have to store those values
 # If I have the time I can manage it by indexing on it. 
-#k = (float)2/(22+1)
+k = float(2)/(22+1)
+# we take the closing prices for the first 22 days
+# closingPricesBTC = df['BTC'].iloc[1:22].mean()
+closingPricesETH = df['ETH'].iloc[1:22].mean()
+closingPricesDASH = df['DASH'].iloc[1:22].mean()
+# taking the following day s (day 23) closing price multiplied by k, then multiply the previous day s moving average by (1-k) and add the two.
+emaBTC={}
+for i in range(0, len(df)-24):
+	closingPricesBTC = df['BTC'].iloc[1+i:22+i].mean()
+	emaBTC[i]= df['BTC'].iloc[23 + i]*k+closingPricesBTC*(1-k)
 
-
-
+print emaBTC
 # on lance une alerte d'achat ou de vente en cas de pb
 result = np.array([["",'BTC','ETH','DASH'],
 						['Action',"","",""]])
