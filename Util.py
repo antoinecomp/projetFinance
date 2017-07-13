f76c24a7b59fe090eebc432cfbb0aa185e882c4c
Tony <antoine.compagnie@gmail.com>
2017-07-13 18:06:29 +0200

    creation inachevee d un exemple en local

--- Util.py
+++ Util.py
@@ -1,18 +1,59 @@
 import threading
 from Elastic import Elastic
 import urllib.request
-from ValueAnalyse import ValueAnalyse 
+from ValueAnalyse import ValueAnalyse
+import json
+ 
 class Util:
 	def __init__(self):
 		pass
 	def disp(self,el,call,prices,assets):
-		threading.Timer(1, self.disp,[el,call,prices,assets]).start()
-		value = urllib.request.urlopen(call).read()
-		prices.append(value)
-		print(len(prices))
-		print("prices : ")
-		print(type(prices))
-		a = " ".join(str(x) for x in prices)
-		print(a)
+		#threading.Timer(1, self.disp,[el,call,prices,assets]).start()
+		#value = urllib.request.urlopen(call).read()
+		#prices.append(value)
+		#print(len(prices))
+		#print("prices : ")
+		#print(type(prices))
+		#a = " ".join(str(x) for x in prices)
+		#print(a)
+
+		# test
+		
+		json_text = """
+		[
+		{"BTC":{"USD":2167.85},"ETH":{"USD":167.88},"DASH":{"USD":102.31}},
+		{"BTC":{"USD":2253.12},"ETH":{"USD":177.76},"DASH":{"USD":109.17}},
+		{"BTC":{"USD":2251.47},"ETH":{"USD":177.71},"DASH":{"USD":109.12}},
+		{"BTC":{"USD":2251.47},"ETH":{"USD":177.71},"DASH":{"USD":109.12}},
+		{"BTC":{"USD":2251.47},"ETH":{"USD":177.71},"DASH":{"USD":109.12}},
+		{"BTC":{"USD":2167.85},"ETH":{"USD":167.88},"DASH":{"USD":102.31}}
+		]
+		"""
+		a = json.loads(json_text)
+		#
 		va = ValueAnalyse(a)
+
+		results_array = va.actionDecision()
+		buyValue =(len(results[1])-1)*[""]
+		sellValue = (len(results[1])-1)*[""]
+	
+		# on parcourt la deuxieme ligne
+		for i in range(1,len(results[1])):
+			if results[1][i] == "buy":
+				buyExist = True
+				# il me faut l'indice a acheter
+				buyValue[k] = results[0][i]
+				k=k+1
+			if results[1][i] == "sell":
+				sellExist = True
+				sellValue[j] = results[0][i]
+				j=j+1
+			#if ((sellExist != 0) and (buyExist !=0)):
+			#	break
+
+		print("Vendre ")
+		print(sellValue)
+		print(" contre ")
+		print (buyValue)		
+
 		el.store(assets,value)
